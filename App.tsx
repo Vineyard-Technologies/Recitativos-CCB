c5e2e55b7164d535bba5a9b98e0d666706288242
Andrew Rogers <Laserwolve@gmail.com>
2025-04-22 15:57:15 -0400

    Added 'add' button

--- App.tsx
+++ App.tsx
@@ -1,7 +1,9 @@
-import { StyleSheet, Text, View, StatusBar } from 'react-native';
+import { StyleSheet, Text, View, StatusBar, TouchableOpacity, Image } from 'react-native';
 import { useFonts } from 'expo-font';
 
 export default function App() {
+  console.log("hello world");
+  
   const [fontsLoaded] = useFonts({
     'CormorantGaramond-SemiBold': require('./assets/fonts/CormorantGaramond-SemiBold.ttf'),
   });
@@ -14,6 +16,16 @@ export default function App() {
     <View style={styles.container}>
       <StatusBar barStyle="default" />
       <Text style={styles.header}>Meus Recitativos</Text>
+      <TouchableOpacity
+        style={styles.addButton}
+        onPress={() => {}}
+        activeOpacity={0.7}
+      >
+        <Image 
+          source={require('./src/components/plus-solid.png')} 
+          style={[styles.addIcon, { tintColor: '#00FF00', alignSelf: 'center' }]}
+        />
+      </TouchableOpacity>
     </View>
   );
 }
@@ -23,6 +35,7 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: '#FFFFFF',
   },
   header: {
     position: 'absolute',
@@ -30,5 +43,20 @@ const styles = StyleSheet.create({
     left: 10,
     fontSize: 36,
     fontFamily: 'CormorantGaramond-SemiBold',
+  },
+  addButton: {
+    position: 'absolute',
+    top: '90%',
+    alignSelf: 'center',
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+    backgroundColor: '#000000',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  addIcon: {
+    width: 24,
+    height: 24,
   },
 });
--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# Recitativos
+### A Mnemonic Portuguese Scripture Memorizer App
 
-Mnemonic Portuguese Scripture Memorizer App
+![Recitativos CCB Logo](https://github.com/Vineyard-Technologies/Recitativos-CCB/blob/main/src/images/icon.png)
