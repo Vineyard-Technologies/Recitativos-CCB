7f50e62547e89f1149f83b2c299c212111a01aac
Andrew Rogers <Laserwolve@gmail.com>
2025-05-13 14:21:08 -0400

    EscolhaUmLivro Screen

---
+++ src/screens/EscolhaLivroScreen.tsx
@@ -0,0 +1,77 @@
+import React from "react";
+import styled from "styled-components/native";
+import Button from "../../components/Button";
+import NavBar from "../components/NavBar";
+import ARC from "../../assets/ARC.json";
+import { ArrowFatLeft } from "phosphor-react-native";
+
+
+const books = ARC.map((book: { name: string }) => book.name);
+
+const EscolhaLivroScreen = ({ onBack }: { onBack: () => void }) => {
+  return (
+    <Container>
+      <NavBar title="Escolha um livro" />
+      <BookListContainer>
+        <ListBorder />
+        <BookList>
+          {books.map((book) => (
+            <BookItem key={book}>
+              <BookText>{book}</BookText>
+            </BookItem>
+          ))}
+        </BookList>
+        <ListBorder />
+      </BookListContainer>
+      <ButtonRow>
+        <Button label="Voltar" onPress={onBack} icon={<ArrowFatLeft size={24} color="#fff" />} />
+      </ButtonRow>
+    </Container>
+  );
+};
+
+const Container = styled.SafeAreaView`
+  flex: 1;
+  background-color: ${({ theme }) => theme.COLORS.NEUTRAL_LIGHT_MEDIUM};
+`;
+
+const BookListContainer = styled.View`
+  flex: 1;
+  margin-top: 16px;
+  margin-bottom: 96px;
+`;
+
+const BookList = styled.ScrollView`
+  flex-grow: 0;
+`;
+
+const BookItem = styled.TouchableOpacity`
+  width: 100%;
+  padding: 10px;
+  margin-bottom: 16px;
+  border-radius: 16px;
+  background-color: ${({ theme }) => theme.COLORS.NEUTRAL_LIGHT_LIGHT};
+  align-items: center;
+`;
+
+const BookText = styled.Text`
+  ${({ theme }) => theme.HEADING.H2};
+  color: ${({ theme }) => theme.COLORS.NEUTRAL_DARK_DARKEST};
+`;
+
+const ButtonRow = styled.View`
+  flex-direction: row;
+  justify-content: center;
+  position: absolute;
+  bottom: 0px;
+  left: 24px;
+  right: 24px;
+`;
+
+const ListBorder = styled.View`
+  height: 3px;
+  background-color: #000;
+  width: 100%;
+`;
+
+export default EscolhaLivroScreen;
